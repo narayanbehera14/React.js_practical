082e5929db142cfd3732064ee47cb04de50f361d
narayanbehera14 <beheranarayancs222330@gmail.com>
2025-12-19 12:12:49 +0530

    updated

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -21,20 +21,18 @@ export default function Calculator() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold text-center mb-4">React Calculator</h1>
+    <div >
+      <h1 >React Calculator</h1>
 
-      <div className="bg-gray-200 text-right text-xl font-mono p-3 mb-4 rounded-lg h-12 w-64 overflow-x-auto">
+      <div>
         {input || "0"}
       </div>
 
-      <div className="grid grid-cols-4 gap-3">
+      <div>
         {[7, 8, 9, 4, 5, 6, 1, 2, 3, 0].map((num) => (
           <button
             key={num}
-            onClick={() => handleClick(num.toString())}
-            className="bg-gray-300 hover:bg-gray-400 p-3 rounded-lg text-lg font-semibold"
-          >
+            onClick={() => handleClick(num.toString())}          >
             {num}
           </button>
         ))}
@@ -42,23 +40,19 @@ export default function Calculator() {
         {["+", "/", "-", "*"].map((op) => (
           <button
             key={op}
-            onClick={() => handleClick(op)}
-            className="bg-blue-400 hover:bg-blue-500 text-white p-3 rounded-lg text-lg font-bold"
-          >
+            onClick={() => handleClick(op)}          >
             {op}
           </button>
         ))}
 
         <button
           onClick={handleClear}
-          className="bg-red-500 hover:bg-red-600 text-white p-3 rounded-lg text-lg font-bold col-span-2"
         >
           C
         </button>
 
         <button
           onClick={handleCalculate}
-          className="bg-green-500 hover:bg-green-600 text-white p-3 rounded-lg text-lg font-bold col-span-2"
         >
           =
         </button>
@@ -66,3 +60,4 @@ export default function Calculator() {
     </div>
   );
 }
+
---
+++ title/src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Amity University Mumbai</h1>
+        <img 
+        src="https://imgs.search.brave.com/Mg822hIuJR3IlOW1DCiZZemo5ZTtCTqn887O37Ec0R4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2VjLzcz/LzhiL2VjNzM4YjRi/ZmI5MjBjZTgzYTc2/MjFhMjViNzM1ZjUx/LmpwZw"
+        alt="Logo"
+        />
+
+        <nav>
+          <a href="#home">Home</a> 
+          <a href="#about">About</a> 
+        </nav>
+      </header>
+      <section>
+        <h2>Images</h2>
+
+        <img
+          src="https://imgs.search.brave.com/FaKcgEwQclcwQ53xEBqASI-84hKYK4bcsr5J8XfJoos/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/Y29sbGVnZWJhdGNo/LmNvbS9zdGF0aWMv/Y2xnLWdhbGxlcnkv/YW1pdHktdW5pdmVy/c2l0eS1tdW1iYWkt/MzU0MDQyLmpwZw"
+          alt="Image 1"
+        />
+      </section>
+    </div>
+  );
+}
+
+export default App;
+1
